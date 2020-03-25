7e75dc507d34e8f6d50b4325570410ce6b26f051
marcfehr <marc.fehr@gmail.com>
2020-03-25 17:39:40 +0200

    changes content on contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import Head from '../components/head';
@@ -12,21 +13,23 @@ const Contact = () => {
           <h1 className='ui header'>
             <div className='content'>
             <span className='page-title'>
-              Get in touch now.
+              We must act now.
             </span>
               <div className='sub header'>
-                This project has been built in very little time <a href={'mailto:marc.fehr@gmail.com'}>Marc Fehr</a> and is available as an Open Source repository on <a href={'https://gitlab.com/marc.fehr/community-isolation-map'} target={'_blank'} rel={'noopener noreferrer'}>Gitlab</a>. Please share the idea and get in touch if you need any help setting this up for your local community. Any feedback is appreciated!
+                Get in touch with the MUIZ-CAN network and find out more. If you are looking for information about the COVID-19 pandemic, visit our <Link to={'/'}>Information</Link> page.
               </div>
+              <Link to={'/'} className={'ui black icon button'}>
+                <i className={'icon map'}/> Go to the map
+              </Link>
             </div>
           </h1>
-          <div className={'ui yellow message'} style={{fontWeight: 500, fontStyle: 'italic'}}>
-            <i className={'icon external link'} />Read the Tutorial about how to set up your own map <a href={'https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83?source=userActivityShare-f57d26da4972-1584988662&_branch_match_id=689400773593121406'} target={'_blank'} rel={'noopener noreferrer'}>here</a>.
-          </div>
         </div>
       </section>
 
       <section className='ui vertical segment' style={{minHeight: '55vh'}}>
-        <div className='ui text container formcontainer'>
+        <div className='ui text container formcontainer'><div className={'ui yellow message'} style={{fontWeight: 500, fontStyle: 'italic'}}>
+          <i className={'icon external link'} />Read the Tutorial about how to set up your own map <a href={'https://medium.com/@marcfehr/how-to-build-a-fast-and-reliable-community-mapping-tool-with-gatsbyjs-and-firebase-74a0fa6b5b83?source=userActivityShare-f57d26da4972-1584988662&_branch_match_id=689400773593121406'} target={'_blank'} rel={'noopener noreferrer'}>here</a>.
+        </div>
           <h2>Reach out via email</h2>
           <ul>
             <li>
@@ -39,8 +42,6 @@ const Contact = () => {
               </a>
             </li>
           </ul>
-          <h2>Shout out on Social Media</h2>
-          <p>Please share this repository and help people using it.</p>
         </div>
       </section>
     </Layout>
